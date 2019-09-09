b114e09b7f65e1e60581702a0f1aa271484c0e49
Glitch (wildercat-mozilla-gallery-start) <none>
2019-09-09 07:13:09 +0000

    👀🍄 Checkpoint ./main.js:5242288/615

--- main.js
+++ main.js
@@ -3,16 +3,14 @@ var thumbBar = document.querySelector('.thumb-bar');
 
 var btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
-
+console.log('AAA');
 /* Looping through images */
-for (let i = 1; i > 5; i++) {
-  console.log('AAA');
+for (let i = 1; i < 6; i++) {
   var newImage = document.createElement('img');
   newImage.setAttribute('src', 'images/pic' + i + '.jpg');
-  /* From the instructions:
-  "Find the value of the src attribute of the current image. This can be done by running the getAttribute() function on the <img> in each case, and passing it a parameter of "src" in each case."
-  It seems like we already know the src attribute, we literally just made it. Why not use the same formula we just used to make it?
-  */
+  newImage.onclick = function() {
+    console.log(getAttribute('src'));
+  }
   thumbBar.appendChild(newImage);
   
 }
