37e8163fa9a966511da6049dc2c2cac985f41932
Glitch (wildercat-mozilla-gallery-start) <none>
2019-09-09 06:50:17 +0000

    ⛹🏰 Checkpoint ./main.js:5242288/84

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ var btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
+for (let i = 1; i > 5; i++) {
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
   thumbBar.appendChild(newImage);
-
+}
 /* Wiring up the Darken/Lighten button */
